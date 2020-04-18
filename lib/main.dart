388fddb1a8aadd8402ff059d8b4a29799620ead1
Roveri <gabriel.roveri7@gmail.com>
2020-04-18 19:59:21 -0300

    Internet permission added, device orientation locked in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:dart_week_mobile/app/app_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(
-      ModularApp(
-        module: AppModule(),
-      ),
-    );
+Future<void> main() async {
+  runApp(
+    ModularApp(
+      module: AppModule(),
+    ),
+  );
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+}
